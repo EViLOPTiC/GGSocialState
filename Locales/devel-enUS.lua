a925af5611a06a16ea4ae0709ee7f7a656a649c1
gOOvER <info@goover.de>
2014-10-20 05:28:47 +0200

    Added Translations for Webbased Translation, but not activated yet - PART 1

---
+++ Locales/devel-enUS.lua
@@ -0,0 +1,6 @@
+local FOLDER_NAME, private = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(FOLDER_NAME, "enUS", true)
+
+if not L then return end
+
+L.GGSOCIALSTATE_LABEL = "LDB plugin that shows friends and guild list.\n"
